eb3407c9dec8805dbce0b10b269176f655096db1
camilomaximus05 <camilobm05@gmail.com>
2025-02-14 16:32:26 -0500

    chore: final design show shifts

--- shiftbank/resources/views/livewire/public-shift-list-component.blade.php
+++ shiftbank/resources/views/livewire/public-shift-list-component.blade.php
@@ -1,22 +1,27 @@
 <style>
-    .conteiner {
+    body {
         background-color: #f8f9fa;
         display: flex;
         justify-content: center;
         align-items: center;
         height: 100vh;
-    }
-
-    p{
+        width: 100vw;
+        overflow: hidden;
         margin: 0;
     }
 
     .screen {
-        width: 900px;
+        max-width: 1920px;
+        max-height: 1080px;
+        width: 90vw;
+        height: 90vh;
         background: white;
-        border-radius: 10px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        padding: 20px;
+        border-radius: 20px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+        padding: 2vh;
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
     }
 
     .header {
@@ -25,118 +30,196 @@
         align-items: center;
         padding-bottom: 10px;
         border-bottom: 2px solid #ddd;
+        font-size: 2vw;
     }
 
-    .turno-card {
-        background: #fd0d0d;
-        color: white;
+    .header small {
+        font-size: 1.5vw;
+    }
+
+    .content {
+        flex: 1;
+        display: flex;
+        gap: 2vw;
+        margin-top: 2vh;
+        overflow: hidden;
+    }
+
+    .turno-list-container {
+        flex: 1;
         display: flex;
         flex-direction: column;
-        justify-content: center;
-        text-align: center;
-        padding: 30px;
-        border-radius: 5px;
         height: 100%;
-    }
-    .info {
-        color: white;
-        justify-content: center;
-        text-align: center;
-        padding: 8px 10px;
+        overflow: hidden;
     }
 
-    .turno-list .list-group-item {
+    .turno-list {
+        flex-grow: 1;
         display: flex;
         flex-direction: column;
-        text-align: center;
-        justify-content: space-between;
-        align-items: center;
-        font-size: 18px;
-        padding: 10px 15px;
-        border: none;
-        background: #e9ecef;
+        justify-content: space-between; 
     }
 
-    .turno-list .list-group-item:nth-child(odd) {
-        background: #c5c7c9;
+    .turno-header {
+        display: flex;
+        border-radius: 10px;
+        overflow: hidden;
+        margin-bottom: 10px;
+        font-size: 2vw;
+        font-weight: bold;
+        text-align: center;
+    }
+
+    .turno-header div {
+        flex: 1;
+        padding: 1vh;
+        color: white;
+    }
+
+    .turno-title {
+        background-color: #F44336;
+        border-top-left-radius: 10px;
+        border-bottom-left-radius: 10px;
+    }
+
+    .modulo-title {
+        background-color: #343a40;
+        border-top-right-radius: 10px;
+        border-bottom-right-radius: 10px;
+    }
+
+    .list-group-item {
+        font-size: 2vw;
+        padding: 1vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        min-height: calc(100% / 5);
+        border: 1px solid #ddd;
+        border-radius: 10px; 
+        background-color: #f9f9f9;
+        overflow: hidden;
+        box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+    }
+
+    .turno-info {
+        display: flex;
+        height: 50%;
+        border-bottom: 2px solid #ddd;
     }
 
     .highlight_turno {
+        width: 50%;
         background: #d4d1d1;
         color: black;
-        padding: 5px 10px;
-        border-radius: 5px 0 0 0;
-        white-space: nowrap;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 2vw;
+        font-weight: bold;
+        padding: 1vh;
+        border-top-left-radius: 10px;
     }
 
-    .highlight_module{
+    .highlight_module {
+        width: 50%;
         background: #e4dfdf;
         color: black;
-        padding: 5px 10px;
-        border-radius: 0 5px 0 0;
-        white-space: nowrap;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 2vw;
+        font-weight: bold;
+        padding: 1vh;
+        border-top-right-radius: 10px;
     }
 
-    .highlight_name{
-        background: #f10707;
-        color: black;
-        padding: 5px 10px;
-        border-radius: 0 0 5px 5px;
-        white-space: nowrap;
+    .highlight_name {
+        width: 100%;
+        background: #F44336;
+        color: white;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 2vw;
+        font-weight: bold;
+        height: 50%;
+        padding: 1vh;
+        border-bottom-left-radius: 10px;
+        border-bottom-right-radius: 10px;
     }
 
+    .turno-card-container {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+    }
+
+    .turno-card {
+        background: #F44336;
+        color: white;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        padding: 3vh;
+        border-radius: 10px;
+        width: 100%;
+        height: 100%;
+        font-size: 3vw;
+    }
 </style>
-<div class="container">
-    <div class="screen">
-        <div class="header">
-            <h4>MI TURNO</h4>
-            <div>
-                <small>Logo de la entidad</small>
-            </div>
-            <div>
-                <small>09:35 p.m. <br> 09 de octubre 2020</small>
-            </div>
-        </div>
 
-        <div class="row mt-3">
-            <div class="col-md-6">
-                <div class="row mb-2">
-                    <div class="col-md-6 info" style="background: #fd0d0d;">
-                        <p>Turno</p>
-                    </div>
-                    <div class="col-md-6 info" style="background: #811818;">
-                        <p>Modulo</p>
-                    </div>
-                </div>
-                <ul class="list-group turno-list">
-                    <li>
-                        <div class="list-group-item"> 
-                            <div class="row">
-                                <p class="highlight_turno col-md-6">AGF 5</p> 
-                                <p class="col-md-6 highlight_module ">08</p>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <p class="highlight_name" >Juan Perez</p>
-                                </div>
-                            </div>
+<div class="container screen">
+    <div class="header">
+        <h4>MI TURNO</h4>
+        <div>
+            <small>Logo de la entidad</small>
+        </div>
+        <div>
+            <small>{{ now()->format('h:i A') }} <br> {{ now()->format('d \d\e F Y') }}</small>
+        </div>
+    </div>
+
+    <div class="content">
+
+        <div class="turno-list-container">
+            <div class="turno-header">
+                <div class="turno-title">Turno</div>
+                <div class="modulo-title">Módulo</div>
+            </div>
+
+            <ul class="list-group turno-list">
+                @foreach ($turnosEnProceso as $turno)
+                    <li class="list-group-item">
+                        <div class="turno-info">
+                            <div class="highlight_turno">{{ $turno->number }}</div>
+                            <div class="highlight_module">{{ $turno->random_module }}</div>
                         </div>
+                        <div class="highlight_name">{{ $turno->name }}</div>
                     </li>
-                    <li class="list-group-item row"><p class="highlight_turno col-md-6">ODT 5</p> <p class="col-md-6 highlight_module ">04</p> </li>
-                    <li class="list-group-item row"><p class="highlight_turno col-md-6">ODT 5</p> <p class="col-md-6 highlight_module ">04</p> </li>
-                    <li class="list-group-item row"><p class="highlight_turno col-md-6">ODT 5</p> <p class="col-md-6 highlight_module ">04</p> </li>
-                    <li class="list-group-item row"><p class="highlight_turno col-md-6">ODT 5</p> <p class="col-md-6 highlight_module ">04</p> </li>
-                </ul>
-            </div>
-            <div class="col-md-6">
-                <div class="turno-card">
-                    <h3>RTH 4</h3>
-                    <h1>02</h1>
-                    <p>Ruben Tabares</p>
-                </div>
-            </div>
+                @endforeach
+            </ul>
         </div>
 
-        <p class="mt-3 text-muted text-center"></p>
+        <div class="turno-card-container">
+            @if ($turnoActual)
+                <div class="turno-card">
+                    <h3>{{ $turnoActual->number }}</h3>
+                    <h1>{{ $turnoActual->random_module }}</h1>
+                    <p>{{ $turnoActual->name }}</p>
+                </div>
+            @else
+                <div class="turno-card">
+                    <h3>Sin turno en proceso</h3>
+                </div>
+            @endif
+        </div>
     </div>
 </div>
